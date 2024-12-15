619908de47c219018119741414a220c1c45c263b
Leonardo Graciano <leonardoferreiragraciano@gmail.com>
2024-12-15 20:41:45 -0300

    feat: add new sessions

---
+++ a31_methods/src/main.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        // self: &Self
+        // poderia ser '&mut self' ou 'self'
+        self.width * self.height
+    }
+
+    fn can_hold(&self, orther: &Rectangle) -> bool {
+        self.width > orther.width && self.height > orther.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("A área do retângulo é {} pixels quarados", rect1.area());
+
+    println!("Consegue rect1 conter rect2? {}", rect1.can_hold(&rect2));
+
+    //Automatic referencing and deferencing
+    println!("Consegue rect1 conter rect3? {}", rect1.can_hold(&rect3));
+    // println!("Consegue rect1 conter rect3?", rect1.can_hold(rect3));
+}
